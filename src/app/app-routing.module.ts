5baec4a277c3098419833b69b82197136214f6cd
CHERGUI Zakariyae <zakariachergui25@gmail.com>
2020-06-11 15:45:48 +0100

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) }, { path: 'CarDetails', loadChildren: () => import('./car-details/car-details.module').then(m => m.CarDetailsModule) }];
+//const routes: Routes = [{ path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) }, { path: 'CarDetails', loadChildren: () => import('./car-details/car-details.module').then(m => m.CarDetailsModule) }];
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./cars/cars.module").then((m) => m.CarsModule),
+  },
+  {
+    path: "cars",
+    loadChildren: () =>
+      import("./cars/cars.module").then((m) => m.CarsModule),
+  },
+  {
+    path: "car-details/:product",
+    loadChildren: () =>
+      import("./car-details/car-details.module").then(
+        (m) => m.CarDetailsModule
+      ),
+  },
+  {
+    path: "**", // WildCard
+    redirectTo: "404",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
